3d16725c0087a8cebe26f82d94b3859c51a67532
andron13 <andron13@gmail.com>
2024-08-26 12:24:34 +0200

    feat: env test

--- app/shared/authentification/firebase.ts
+++ app/shared/authentification/firebase.ts
@@ -1,13 +1,25 @@
 // firebase.ts
-// import { getAnalytics } from "@firebase/analytics";
-import { getApp, getApps, initializeApp } from "@firebase/app";
+import { initializeApp } from "@firebase/app";
 import { getAuth } from "@firebase/auth";
 
-import { getFirebaseConfig } from "~/get-env";
+const firebaseConfig = {
+  apiKey: "AIzaSyDkdE2tspYLue_0YA7-gYGfDMcg5ArsWJc",
+  authDomain: "graphiql-app-ee075.firebaseapp.com",
+  projectId: "graphiql-app-ee075",
+  storageBucket: "graphiql-app-ee075.appspot.com",
+  messagingSenderId: "1071092674079",
+  appId: "1:1071092674079:web:96e669af4fb60ca3856f2a",
+  measurementId: "G-S9D2REK0X9",
+};
 
-const firebaseConfig = getFirebaseConfig();
-
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 export { app, auth };
+
+// // import { getAnalytics } from "@firebase/analytics";
+// import { getFirebaseConfig } from "~/get-env";
+// const firebaseConfig = getFirebaseConfig();
+// const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+// // const analytics = getAnalytics(app);
+// const auth = getAuth(app);
+// export { app, auth };
