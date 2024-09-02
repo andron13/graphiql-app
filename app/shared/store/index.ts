56d3119dfd538fbf8b912c475f88f58a8fd5c283
andron13 <andron13@gmail.com>
2024-09-02 15:17:11 +0200

    delete: redux

--- app/shared/store/index.ts
+++
@@ -1,7 +0,0 @@
-export { addRequest, clearHistory } from "./history-slice";
-export {
-  selectLastRequest,
-  selectRequestHistory,
-  selectRequestHistoryByType,
-} from "./selectors";
-export type { AppDispatch, RootState } from "./store";
